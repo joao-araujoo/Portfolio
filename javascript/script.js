47f4e1e7f13d7b048d6698abc9bfa5e3c87f04d7
joao-araujoo <joao.araujoo2007@gmail.com>
2023-09-15 19:01:50 -0300

    fixing, planning and refactoring

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,26 @@
-let timeout
+const createIntroDivButton = () => {
+    const divPulse = document.createElement('div')
+    divPulse.className = 'pulse'
 
-const divPulse = document.createElement('div')
-divPulse.className = 'pulse'
-const changeSceneButton = document.createElement('button')
-changeSceneButton.innerText = 'Lets start'
-changeSceneButton.className = 'change-scene'
-
-setTimeout(() => {
-    divPulse.appendChild(changeSceneButton)
-    document.querySelector('.intro').appendChild(divPulse)
-    document.querySelector('.pulse').addEventListener('click', () => {
+    divPulse.addEventListener('click', () => {
         document.querySelector('.intro').remove()
         document.querySelector('.container').style.display = 'inline'
         document.body.style.backgroundColor = 'var(--primary-color)'
     })
+
+    createIntroButton(divPulse)
+    document.querySelector('.intro').appendChild(divPulse)
+}
+
+const createIntroButton = (div) => {
+    const changeSceneButton = document.createElement('button')
+    changeSceneButton.innerText = 'Lets start'
+    changeSceneButton.className = 'change-scene'
+
+    div.appendChild(changeSceneButton)
+}
+
+// TODO aumentar o timeout para 7000-8000 quando finalizar
+setTimeout(() => {
+    createIntroDivButton()
 }, 1000)
