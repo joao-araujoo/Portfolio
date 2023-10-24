e42388bc2fb90b1c8951bdc9934e1ba965b0c00e
joao-araujoo <joao.araujoo2007@gmail.com>
2023-10-24 19:02:50 -0300

    feature: media icons and fixings

--- javascript/switch-theme.js
+++ javascript/switch-theme.js
@@ -1,4 +1,5 @@
 const root = document.querySelector(':root')
+const slider = document.querySelector('.slider')
 
 const changeRootProperty = (primaryColor, secondaryColor, fontColor, selectionColor) => {
     root.style.setProperty('--primary-color', primaryColor)
@@ -7,10 +8,17 @@ const changeRootProperty = (primaryColor, secondaryColor, fontColor, selectionCo
     root.style.setProperty('--selection-color', selectionColor)
 }
 
-document.querySelector('.slider').addEventListener('click', () => {
+const changeButtonIcons = (githubIcon, linkedinIcon) => {
+    document.querySelector('.github-button img').src = githubIcon
+    document.querySelector('.linkedin-button img').src = linkedinIcon
+}
+
+slider.addEventListener('click', () => {
     if(document.querySelector('input.toggle-input').checked){
-        changeRootProperty('#fff', '#000', '#4b4b4b', 'rgba(206, 206, 206, 0.8)')
+        changeRootProperty('#fff', '#141414', '#000', 'rgba(206, 206, 206, 0.8)')
+        changeButtonIcons('./assets/icons/github light.png', './assets/icons/linkedin light.png')
     } else {
-        changeRootProperty('#000', '#fff', '#ccc', 'rgba(53, 53, 53, 0.8)')
+        changeRootProperty('#141414', '#fff', '#fafafa', 'rgba(53, 53, 53, 0.8)')
+        changeButtonIcons('./assets/icons/github dark.png', './assets/icons/linkedin dark.png')
     }
 })
