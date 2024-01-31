91bd68a1eb88b0a559fcbe118a66302798171227
joao araujo <joao.araujoo2007@gmail.com>
2024-01-31 20:18:29 -0300

    feat: add 404 error page

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 ## Todo
 
-- Componentizar
+- Componentizar Organisms e Templates (Root Layout)
 - Refatorar
 - Adicionar hover aos header links
 - Procurar outra API de notícias ou fazer outra coisa para o corinthians
 - Melhorar o display de filmes
 - Criar modal dos projetos
+- Criar página de obrigado ao realizar contato e de 404
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,17 @@
+import Particle from "../components/Atoms/Particle/Particle";
+import Main from "../components/Organisms/Main";
+
 export default function Error() {
   document.title = "404 | Not Found";
 
   return (
-    <div>Error</div>
+    <>
+      <Particle />
+        <h1 className="background-title">Oops.</h1>
+        <Main footerText="Go Back Home" footerPath="/">
+          <h1 className="title" style={{ fontSize: "100px" }}>404</h1>
+          <h1 className="title" style={{ marginBottom: "50px" }}>Page Not Found.</h1>
+      </Main>
+    </>
   )
 }
