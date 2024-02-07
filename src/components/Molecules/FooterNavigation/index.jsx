2f3b580d765cea7a63006e2d0391a8e151997e3c
joao araujo <joao.araujoo2007@gmail.com>
2024-02-07 15:40:58 -0300

    feat: add whatsapp and curriculum to social links

--- src/components/Molecules/FooterNavigation/index.jsx
+++ src/components/Molecules/FooterNavigation/index.jsx
@@ -1,6 +1,9 @@
 import AnchorLink from "../../Atoms/AnchorLink";
 import styles from "./styles.module.css";
-import { FaGithub, FaLinkedin, FaInstagram, FaSpotify } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaInstagram, FaSpotify, FaWhatsapp } from "react-icons/fa";
+import { HiDocumentArrowDown } from "react-icons/hi2";
+import { HiOutlineDocumentArrowDown } from "react-icons/hi2";
+
 
 export default function FooterNavigation() {
   return (
@@ -12,11 +15,20 @@ export default function FooterNavigation() {
         <AnchorLink path="https://www.linkedin.com/in/joao-araujoo/">
           <FaLinkedin color="#c6c6c6" />
         </AnchorLink>
+        <AnchorLink path="https://api.whatsapp.com/send/?phone=5512997272631">
+          <FaWhatsapp color="#c6c6c6" />
+        </AnchorLink>
         <AnchorLink path="https://www.instagram.com/joao.araujoo_/">
           <FaInstagram color="#c6c6c6" />
         </AnchorLink>
         <AnchorLink path="https://open.spotify.com/user/uxb66upc7ekxdup54a4frat8e?si=992f8ee8344f4d95">
           <FaSpotify color="#c6c6c6" />
+        </AnchorLink>
+        <AnchorLink
+          path="/Curriculo - João Pedro Araujo Costa.pdf"
+          download="CV - João Pedro Araujo Costa"
+        >
+          <HiOutlineDocumentArrowDown color="#c6c6c6" />
         </AnchorLink>
       </div>
     </nav>
